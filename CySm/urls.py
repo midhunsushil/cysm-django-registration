859d878955223a4784aac2f19bf1d70a8c41ed52
BuildTools <unconfigured@null.spigotmc.org>
2020-10-17 12:01:51 +0530

    -Set main landing page to index page

--- CySm/urls.py
+++ CySm/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from my_first_app import views
+from registration import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path('', views.login, ),
-    path('my_first_app/', include('my_first_app.urls')),
+    path('', views.index),
+    path('login/', include('my_first_app.urls')),
     path('register/', include('registration.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #Comment out after after changing to production mode
