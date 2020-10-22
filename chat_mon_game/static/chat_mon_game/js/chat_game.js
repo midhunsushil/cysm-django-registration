a892f4d9e41ea20d80852ed908450a8c464786a4
BuildTools <unconfigured@null.spigotmc.org>
2020-10-22 21:33:26 +0530

    -Response for game chat is now from csv file in media folder

--- chat_mon_game/static/chat_mon_game/js/chat_game.js
+++ chat_mon_game/static/chat_mon_game/js/chat_game.js
@@ -1,8 +1,28 @@
 $(document).ready(function () {
 
+// Acquire CSRF Token
+  function getCookie(name) {
+      let cookieValue = null;
+      if (document.cookie && document.cookie !== '') {
+          const cookies = document.cookie.split(';');
+          for (let i = 0; i < cookies.length; i++) {
+              const cookie = cookies[i].trim();
+              // Does this cookie string begin with the name we want?
+              if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+  }
+  const csrftoken = getCookie('csrftoken');
+
 setInterval(function () {
   $.ajax({
+        headers: {'X-CSRFToken': csrftoken},
         url: 'getchat/',
+        type: 'POST',
         success: function (data) {
           if (data) {
             console.log(data);
--- chat_mon_game/views.py
+++ chat_mon_game/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
+from django.conf import settings
+import random, os, csv
 
 #Variable
 global_count = 0
@@ -17,6 +19,42 @@ def game(request) :
 
 def getChat(request) :
 
-        global global_count
-        global_count = global_count + 1
-        return HttpResponse(global_count)
+        if request.method == "POST":
+            global global_count
+            global_count = global_count + 1
+            return HttpResponse(getrandomChat())
+
+        return HttpResponseForbidden()
+
+def getrandomChat() :
+
+    path = os.path.join(settings.BASE_DIR, "media", "chat_mon_game")
+    # csv file name
+    filename = "chatlog.csv"
+    # csv file path
+    csvfilepath = os.path.join(path, filename)
+
+    # initializing the titles and rows list
+    fields = []
+    rows = []
+    no_of_rows = 0
+
+    # reading csv file
+    with open(csvfilepath, 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+
+        # get total number of rows
+        no_of_rows = csvreader.line_num-1
+        print("Total no. of chats read:", no_of_rows)
+
+    randChat = random.randint(0,no_of_rows-1)
+    print("randchat:", randChat)
+    return rows[randChat][2]
