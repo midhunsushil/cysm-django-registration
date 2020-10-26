6730136f07f3dbebfa884d409fab53b73b8f7ab7
BuildTools <unconfigured@null.spigotmc.org>
2020-10-26 20:02:23 +0530

    -Added click event to close-btn -Chat will be displayed only on webpage focus -Improved selected chat-bubble highlight feature

--- chat_mon_game/static/chat_mon_game/js/chat_game.js
+++ chat_mon_game/static/chat_mon_game/js/chat_game.js
@@ -20,12 +20,16 @@ $(document).ready(function() {
 
   var selectedChats = new Array()
 
-  function chatInfoTemplateClone(from, text) {
+  function chatInfoTemplateClone(slno, from, text) {
 
-    console.log("Cloning template")
+    console.log("Cloning chatInfo template")
     var templateContent = document.querySelector('template#chatInfo').content;
-    $(templateContent).find("#newPost .content-info .accountName").text(from);
-    $(templateContent).find("#newPost .message").text(text);
+    $(templateContent).find(".newPost .content-info .accountName").text(from);
+    $(templateContent).find(".newPost .content-info").attr({
+      "chat_data_slno": slno
+    })
+    $(templateContent).find(".newPost .message").text(text);
+
 
     var targetContainer = document.querySelector('.jumbotron.chat-info');
     targetContainer.appendChild(document.importNode(templateContent, true));
@@ -39,63 +43,93 @@ $(document).ready(function() {
 
     var targetContainer = document.querySelector('div.chat-container');
     targetContainer.appendChild(document.importNode(templateContent, true));
+    $(targetContainer).find(".bubble").removeClass("current");
     $(targetContainer).find(".bubble:last-child").addClass(classes);
 
   }
 
+  function addSelected(slno) {
+    console.log("Adding selected class");
+    if ($.inArray(slno, selectedChats) == -1) {
+      selectedChats.push(slno);
+    }
+    var input_info_slno = "input[name='info'][chat_data_slno='" + slno + "' ]";
+    var $bubbleElement = $('.chat-container .bubble').find(input_info_slno);
+    $bubbleElement.parent().addClass("selected");
+  }
+
+  function removeSelected(slno) {
+    console.log("Removing selected class");
+    if ($.inArray(slno, selectedChats) == -1) {
+      console.log("Unable to remove selected !");
+      return 0
+    } else {
+      selectedChats = $.grep(selectedChats, function(value) {
+        return value != slno;
+      });
+    }
+    var input_info_slno = "input[name='info'][chat_data_slno='" + slno + "' ]";
+    var $bubbleElement = $('.chat-container .bubble').find(input_info_slno);
+    $bubbleElement.parent().removeClass("selected");
+    return 1;
+  }
+
   setInterval(function() {
-    $.ajax({
-      headers: {
-        'X-CSRFToken': csrftoken
-      },
-      url: 'getchat/',
-      type: 'POST',
-      success: function(data) {
-        if (!data.errorExist) {
+    if (document.hasFocus()) {
+      $.ajax({
+        headers: {
+          'X-CSRFToken': csrftoken
+        },
+        url: 'getchat/',
+        type: 'POST',
+        success: function(data) {
+          if (!data.errorExist) {
 
-          console.log(data);
+            console.log(data);
 
-          if($.inArray(data.slno, selectedChats) == -1) {
-            chatBubbleTemplate(data.chat, "current");
+            if ($.inArray(data.slno, selectedChats) == -1) {
+              chatBubbleTemplate(data.chat, "current");
+            } else {
+              chatBubbleTemplate(data.chat, "current selected");
+            }
+
+            $('<input>').attr({
+              type: 'hidden',
+              name: 'info',
+              'chat_data_slno': data.slno,
+              'chat_data_from': data.from,
+              'chat_data_text': data.chat,
+              'chat_data_mod': data.moderation
+            }).appendTo('.chat-container .current');
+
+            var $chatContainer = $('.chat-container')[0]
+            var chat_scrollTop = $chatContainer.scrollTop
+            var chat_scrollHeight = $chatContainer.scrollHeight
+            var chat_clientHeight = $chatContainer.clientHeight
+
+            console.log("scrollTop:" + chat_scrollTop)
+            console.log("scrollHeight:" + chat_scrollHeight)
+
+            $(".chat-container").animate({
+              scrollTop: $(
+                '.chat-container').get(0).scrollHeight
+            }, 800);
+
+          } else {
+            console.log("No data");
           }
-          else {
-            chatBubbleTemplate(data.chat, "current selected");
-          }
-
-          $('<input>').attr({
-            type: 'hidden',
-            name: 'info',
-            'chat_data_slno': data.slno,
-            'chat_data_from': data.from,
-            'chat_data_text': data.chat,
-            'chat_data_mod': data.moderation
-          }).appendTo('.chat-container .current');
-
-          var $chatContainer = $('.chat-container')[0]
-          var chat_scrollTop = $chatContainer.scrollTop
-          var chat_scrollHeight = $chatContainer.scrollHeight
-          var chat_clientHeight = $chatContainer.clientHeight
-
-          console.log("scrollTop:" + chat_scrollTop)
-          console.log("scrollHeight:" + chat_scrollHeight)
-
-          $(".chat-container").animate({
-            scrollTop: $(
-              '.chat-container').get(0).scrollHeight
-          }, 800);
-
-        } else {
-          console.log("No data");
         }
-      }
-    });
+      });
+    }
   }, 3000);
 
   $(".chat-container::-webkit-scrollbar-thumb").on("mousedown", function() {
     console.log("Pressed");
   });
 
+  //Chat bubble clicked/selected
   $(".chat-container").on("click", ".bubble:not(.selected)", function() {
+
     console.log('Clicked Chat!')
 
     $inputInfo = $(this).children("input[name = 'info']")
@@ -108,12 +142,24 @@ $(document).ready(function() {
       mod: $inputInfo.attr('chat_data_mod')
     }
 
-    selectedChats.push(data.slno)
-    $(this).addClass('selected')
-    chatInfoTemplateClone(data.from, data.text);
+    addSelected(data.slno);
+    chatInfoTemplateClone(data.slno, data.from, data.text);
+  });
+  //Chat bubble clicked/selected END
+
+  //Close Button clicked on chatInfo
+  $("div").on("click", ".newPost .nav-item .close-btn", function() {
+
+    $container = $(this).parents("div.newPost")
+    slno = $container.find(".content-info").attr("chat_data_slno");
+    console.log("Close btn clicked #chatInfo slno = " + slno);
+    var removed = removeSelected(slno);
+    if (removed) {
+      $container.remove();
+    }
   });
 
-  if($('.chat-container')[0].scrollHeight - $('.chat-container')[0].scrollTop > 500 ) {
+  if ($('.chat-container')[0].scrollHeight - $('.chat-container')[0].scrollTop > 500) {
     $('.chat-container')[0].scrollTop = $('.chat-container')[0].scrollHeight
   }
 });
